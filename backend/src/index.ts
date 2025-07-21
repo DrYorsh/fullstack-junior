052afe00d7d17d4c1c59d54f3de857ce60573457
Pit Yersh <pitnkz1@gmail.com>
2025-07-21 13:00:37 +0700

    second-fix-CORS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 var corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'http://compitplus.ru/'],
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 app.use(cors(corsOptions));
